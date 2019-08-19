1361837bd354b2e60910c841200a86a4a7ec703c
Ahmad Majid <Ahmadmajid1994@gmail.com>
2019-08-19 14:20:31 +0100

    changed some imports

--- app/javascript/components/select.js
+++ app/javascript/components/select.js
@@ -1,12 +1,8 @@
 import $ from "jquery";
 import select2 from "select2";
 
-import 'select2/dist/css/select2.css';
-
 const improveDropdown = () => {
-  $(document).ready(function() {
-    $('.beautiful-dropdown').select2();
-  });
+  $('.beautiful-dropdown').select2();
 }
 
 export default improveDropdown;
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,4 +1,5 @@
 import 'bootstrap';
-import improveDropdown from '../components/select';
+import improveDropdown from './../components/select';
+import 'select2/dist/css/select2.css';
 
 improveDropdown();
